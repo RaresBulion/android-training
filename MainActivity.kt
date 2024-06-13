3019eeff98fe220bf78e9768f0551c4ce1e3ce84
RaresBulion <lupuianrares@yahoo.com>
2024-06-13 16:56:51 +0300

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,79 +1,188 @@
-package com.example.taskcompleted
+package com.example.lemonade
 
 import android.os.Bundle
+import android.service.autofill.OnClickAction
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.taskcompleted.ui.theme.TaskCompletedTheme
+import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TaskCompletedTheme {
+            LemonadeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TaskManagerApp()
+                   LemonadeMakingApp()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TaskManagerApp(){
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        val image = painterResource(R.drawable.ic_task_completed)
-        Image(painter = image, contentDescription = null)
-        Text(
-            text = stringResource(R.string.firstmsg),
-            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = stringResource(R.string.secondmsg),
-            fontSize = 16.sp
-        )
-
+fun LemonadeMakingApp(modifier: Modifier = Modifier) {
+    var currentStep by remember { mutableStateOf(1) }
+    var squeezeCount by remember {
+        mutableStateOf(0)
     }
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lemonade",
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        }
+    ) {innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            when (currentStep) {
+
+                1 -> {
+                    Column(
+                        modifier = modifier
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    )
+                    {
+                        Image(
+                            painter = painterResource(id = R.drawable.lemon_tree),
+                            contentDescription = stringResource(
+                                id = R.string.lemon_tree_content_description
+                            ),
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .clickable {
+                                    currentStep = 2
+                                    squeezeCount = (2..4).random()
+                                }
+                        )
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(text = stringResource(R.string.first_step))
 
 
+                    }
+                }
+                2 -> {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ){
+                        Image(
+                            painter = painterResource(id = R.drawable.lemon_squeeze),
+                            contentDescription = stringResource(id = R.string.lemon_content_description),
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .clickable {
+                                    squeezeCount--
+                                    if (squeezeCount == 0) {
+                                        currentStep = 3
+                                    }
+                                }
+                        )
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(text = stringResource(R.string.second_step))
 
+                    }
+                }
+                3 -> {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.lemon_drink),
+                            contentDescription = stringResource(id = R.string.lemonade_glass_content_description),
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .clickable { currentStep = 4 }
+
+                        )
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(text = stringResource(id = R.string.third_step))
+
+                    }
+                }
+                4 -> {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.lemon_restart),
+                            contentDescription = stringResource(id = R.string.lemonade_glass_content_description),
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .clickable { currentStep = 1 })
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(text = stringResource(id = R.string.fourth_step))
+
+                    }
+                }
+
+            }
+        }
+    }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    TaskCompletedTheme {
-        TaskManagerApp()
+    LemonadeTheme {
+        LemonadeMakingApp(modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center))
+
     }
 }
